c4008080861e07ca2b1ecfdac989927d6c70239a
Janick Weberpals, RPh, PhD <83728221+janickweberpals@users.noreply.github.com>
2025-04-13 18:54:16 +0200

    restructured to match new outline

--- _pre_render.R
+++ _pre_render.R
@@ -3,9 +3,16 @@
 # Load required packages
 library(quarto)
 
+# render GitHub Copilot presentation
+quarto::quarto_render(
+  input = "03a_copilot_presentation.qmd",
+  output_format = "revealjs",
+  output_file = "03a_copilot_presentation.html"
+  )
+
 # render Quarto presentation
 quarto::quarto_render(
   input = "04a_quarto_presentation.qmd",
   output_format = "revealjs",
-  output_file = "04a_quarto_presentation.html"
+  output_file = "05a_quarto_presentation.html"
   )
