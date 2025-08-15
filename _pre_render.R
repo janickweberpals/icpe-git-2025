8e4feea37f33baa7296b3e4ddf9fefb0ed57e11a
Janick Weberpals, RPh, PhD <83728221+janickweberpals@users.noreply.github.com>
2025-08-15 11:35:54 +0200

    changed ordering and added slide for course evaluation

--- _pre_render.R
+++ _pre_render.R
@@ -12,9 +12,9 @@ quarto::quarto_render(
 
 # render GitHub Copilot presentation
 quarto::quarto_render(
-  input = "03a_copilot_presentation.qmd",
+  input = "04a_copilot_presentation.qmd",
   output_format = "revealjs",
-  output_file = "03a_copilot_presentation.html"
+  output_file = "04a_copilot_presentation.html"
   )
 
 # render Quarto presentation
